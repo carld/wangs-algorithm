8c0677561d1ed0e4d3a1749454d0088c99324e28
Carl <carl.douglas@gmail.com>
2017-06-20 16:04:55 -0400

    Improve the output at each step

--- wang.rkt
+++ wang.rkt
@@ -1,41 +1,48 @@
 #lang racket
 
-(define output
-  (let [(line 0)
-        (width 50)]
-    (lambda (sequent rule . notes)
-      (set! line (+ line 1))
-      (let [(seqstr (format "~a" sequent))]
-        (printf "~a\t~a ~a ~a ~a~%" line seqstr (make-string (- width (string-length seqstr)) #\space) rule notes)))))
+(define (sequent->string sequent)
+  (let* [(seq-as-str (format "~a" sequent))
+         (sequent-max-width 50)
+         (padding    (make-string (- sequent-max-width (string-length seq-as-str)) #\space))]
+    (format "~a~a" sequent padding)))
+
+(define wang-eval0
+  (case-lambda
+    [(sequent rule line)
+     (printf "~a\t~a ~a applied to ~a~%" line (sequent->string sequent) rule (- line 1))
+     (wang-eval (+ 1 line) sequent)]
+    [(sequent line)
+     (printf "~a\t~a~%" line (sequent->string sequent))
+     (wang-eval (+ 1 line) sequent)]))
 
 (define wang-eval
   (case-lambda
     [(sequent)
-     (wang-eval sequent 'start)]
-    [(sequent rule)
-     (output sequent rule)
+     (wang-eval0 sequent 1)]
+    [(line sequent)
      (match-let ([`(,lhs ... => ,rhs ...) sequent])
        (cond
          ; rule 6: proved if formula occurs on both left and right sides aka an axiom
          [(not (null? (set-intersect lhs rhs)))    #t]
          [else
           (match sequent
-            ; rule 1: not on the left; not on the right
-            [`(,p ... (not ,q) ,r ... => ,s ...)                   (wang-eval `(,@p ,@r => ,@s ,q) 'R1)]
-            [`(,p ... => ,prop (not ,q))                           (wang-eval `(,@p ,q => ,prop) 'R1)]
-            ; rule 2: and on the left; or on the right
-            [`(,p ... (,x and ,y) ,s ... => ,prop ...)             (wang-eval `(,@p ,x ,y ,@s => ,@prop) 'R2)]
-            [`(,p ... => ,prop ... (,x or ,y))                     (wang-eval `(,@p => ,@prop ,x ,y)  'R2)]
-            ; rule 3: splitting on the left AKA or on the left
-            [`(,p ... (,x or ,y) ,s ... => ,prop ...)              (let [(s1 (wang-eval `(,@p ,x ,@s => ,@prop)  'R3))
-                                                                         (s2 (wang-eval `(,@p ,y ,@s => ,@prop)  'R3))]
-                                                                     (and s1 s2))]
-            ; rule 4: splitting on the right AKA and on the right
-            [`(,p ... => ,prop ... (,x and ,y) ,r ...)             (let [(s1 (wang-eval `(,@p => ,@prop ,x ,@r)  'R4))
-                                                                         (s2 (wang-eval `(,@p => ,@prop ,y ,@r)  'R4))]
-                                                                     (and s1 s2))]
+            ; rule 1: NOT on the left; NOT on the right
+            [`(,p ... (not ,q) ,r ... => ,rhs ...)                          (wang-eval0 `(,@p ,@r   => ,@rhs ,q) 'R1 line)]
+            [`(,lhs ...               => ,p ... (not ,q) ,r ...)            (wang-eval0 `(,@lhs ,q  => ,@p ,@r) 'R1 line)]
+            ; rule 2: AND on the left; OR on the right
+            [`(,p ... (,q and ,r) ,s ... => ,rhs ...)                       (wang-eval0 `(,@p ,q ,r ,@s => ,@rhs) 'R2 line)]
+            [`(,lhs ...                  => ,p ... (,q or ,r) ,s ...)       (wang-eval0 `(,@lhs => ,@p ,q ,r ,@s)  'R2 line)]
+            ; rule 3: splitting on the left, OR on the left
+            [`(,p ... (,q or ,r) ,s ...  => ,rhs ...)                       (let [(s1 (wang-eval0 `(,@p ,q ,@s => ,@rhs)  'R3 line))
+                                                                                  (s2 (wang-eval0 `(,@p ,r ,@s => ,@rhs)  'R3 line))]
+                                                                              (and s1 s2))]
+            ; rule 4: splitting on the right, AND on the right
+            [`(,lhs ...          => ,p ... (,q and ,r) ,s ...)              (let [(s1 (wang-eval0 `(,@lhs => ,@p ,q ,@s)  'R4 line))
+                                                                                  (s2 (wang-eval0 `(,@lhs => ,@p ,r ,@s)  'R4 line))]
+                                                                              (and s1 s2))]
             ; rule 5: eliminate implication
-            [`(,s ... (,p -> ,q) ,t ... => ,prop ...)              (wang-eval `(,@s ((not ,p) or ,q) ,@t => ,@prop)  'R5)]
+            [`(,p ... (,q -> ,r) ,s ...  => ,rhs ...)                         (wang-eval0 `(,@p ((not ,q) or ,r) ,@s => ,@rhs)  'R5 line)]
+            [`(,lhs ...                  => ,p ... (,q -> ,r) ,s ...)         (wang-eval0 `(,@lhs => ,@p ((not ,q) or ,r) ,@s)  'R5 line)]
             ; rule 7: invalid if no connectives and no symbol occurs on both sides
             [_ #f])]))]))
 
@@ -50,3 +57,4 @@
 (wang-eval `(Q R => (not P) R))
 (wang-eval `(Q => (not P) R Q))
 (wang-eval `((P -> Q) (Q -> R) (not R) => (not P)))
+(wang-eval `(((P -> Q) -> R) (not Q) => (not R)))
